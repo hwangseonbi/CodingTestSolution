20dbbed639eeef89fda77fedbdd0e16d31abfeda
hwangseonbi <on91716@gmail.com>
2021-06-01 15:11:33 +0900

    문제 추가 - Backjoon 2178 (https://www.acmicpc.net/problem/2178)
    문제 업데이트
    - Programmers 43165 (https://programmers.co.kr/learn/courses/30/lessons/43165)
      기존 조합을 이용한 풀이에서 BFS 풀이로 수정

--- solutions/programmers/solutions/43165.py
+++ solutions/programmers/solutions/43165.py
@@ -1,16 +1,38 @@
 import itertools
 
+# def solution(numbers, target):
+#     answer = 0
+#
+#     numbers_index = [i for i in range(len(numbers))]
+#     _sum = sum(numbers)
+#     for i in range(len(numbers)):
+#         for minus_index in itertools.combinations(numbers_index, i):
+#             minus_sum = 0
+#             for j in minus_index:
+#                 minus_sum += numbers[j]
+#             if (_sum - minus_sum * 2) == target:
+#                 answer += 1
+#
+#     return answer
+
+
+answer = 0
+
+
+def DFS(depth, numbers, target, result):
+    global answer
+
+    if depth >= len(numbers):
+        if result == target:
+            answer += 1
+            print(result)
+    else:
+        DFS(depth + 1, numbers, target, result - numbers[depth])
+        DFS(depth + 1, numbers, target, result + numbers[depth])
+
+
 def solution(numbers, target):
-    answer = 0
-
-    numbers_index = [i for i in range(len(numbers))]
-    _sum = sum(numbers)
-    for i in range(len(numbers)):
-        for minus_index in itertools.combinations(numbers_index, i):
-            minus_sum = 0
-            for j in minus_index:
-                minus_sum += numbers[j]
-            if (_sum - minus_sum * 2) == target:
-                answer += 1
-
+    global answer
+    DFS(0, numbers, target, 0)
     return answer
+
